77a8027beb9973448023efc5de2409eff3ad9246
佐藤現 <satougen@satougennoMacBook-Pro.local>
2020-10-30 11:25:49 +0900

    lesson 1 file add

---
+++ README.md
@@ -0,0 +1,8 @@
+# Scala 基礎学習
+
+## ファイル説明
+### part1_basic
+- FirstScalaObject  
+Scalaのデータ型の基礎
+
+-
---
+++ src/part1_basic/FirstScalaObject.scala
@@ -0,0 +1,24 @@
+package part1_basic
+
+case object FirstScalaObject extends App{
+  val x: Int = 42
+  print(x)
+
+//  変数宣言時は型を指定せずとも宣言できる
+  val name: String = "Sato"
+  val firstName = "Gen"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val sShort: Short = 4613
+  val aLog: Long = 52455555555555L
+  val aFloa: Float = 2.0f
+  val aDouble: Double = 3.14
+
+  var aVariable: Int = 4
+  aVariable = 5
+
+/* var name: type = value*/
+/*　で宣言　*/
+}
